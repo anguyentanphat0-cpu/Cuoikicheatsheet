e4cc70f643be4f0ef2fc1d00a582218f25495110
anguyentanphat0-cpu <anguyentanphat0@gmail.com>
2025-11-21 22:07:33 +0700

    Add files via upload

---
+++ Chapter2_Ex3.py
@@ -0,0 +1,14 @@
+width = 17
+height = 12.0
+print('1. width // 2')
+print( width // 2)
+print('Type:', type(width // 2))
+print('\n2. width / 2.0')
+print( width / 2.0)
+print('Type:', type(width / 2.0))
+print('\n3. height / 3')
+print( height / 3)
+print('Type:', type(height / 3))
+print('\n4. 1 + 2 * 5')
+print( 1 + 2 * 5)
+print('Type:', type(1 + 2 * 5))
